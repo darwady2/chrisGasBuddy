6253531d7fe3fad42524ace5f9d32b66e2294468
darwady2 <darwady2@gmail.com>
2017-10-06 00:25:27 -0500

    wrote csv functions

--- chrisGasBuddy.py
+++ chrisGasBuddy.py
@@ -1,3 +1,5 @@
+import datetime
+import csv
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -17,25 +19,57 @@ def get_prices_from_web(html):
 #Cycles through the returned prices and returns a list of the prices.
 def show_prices(prices):
 	complete_prices = []
-	
 	for price in prices:
 		split1_price = str(price).split(">")
 		split2_price = split1_price[1].split("<")
 		complete_prices.append(split2_price[0])
-	
 	return complete_prices
 
 
-
-
-
-def main():
-
+def winnebago():
 	html = get_http('https://www.gasbuddy.com/Station/66970')
 	prices_list = get_prices_from_web(html)
 	price_final = show_prices(prices_list)
-	print price_final[0]
-	print price_final[3]
+	retail_gas = float(price_final[0])
+	mft = .184
+	propane_mft = .226
+	retail_minus_mft = retail_gas - mft
+	sales_tax = .05 * retail_minus_mft
+	retail_taxfree = retail_minus_mft - sales_tax
+	propane_price = retail_taxfree - 0.50
+	raw_cost_of_propane = propane_price - propane_mft
+	current_date = datetime.datetime.now().strftime("%m-%d-%y")
+	filename = 'winnebago_price_' + current_date + '.csv'
+	with open(filename, 'wb') as csvfile:
+		filewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL) #quotechar='|', 
+		filewriter.writerow(['Raw Cost of Propane',raw_cost_of_propane])
+		filewriter.writerow(['WI Propane MFT',propane_mft])
+		filewriter.writerow(['Propane Price',propane_price])
+	
+	return propane_price
+	
+
+
+def rd_diesel():
+	html = get_http('https://www.gasbuddy.com/Station/29546')
+	prices_list = get_prices_from_web(html)
+	price_final = show_prices(prices_list)
+	retail_gas = float(price_final[3])
+	sale_gas = retail_gas - .10
+	current_date = datetime.datetime.now().strftime("%m-%d-%y")
+	filename = 'r&d_diesel_price_' + current_date + '.csv'
+	with open(filename, 'wb') as csvfile:
+		filewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL) #quotechar='|',
+		filewriter.writerow(['Retail Diesel at Falcon Fuel, 300 S Cicero Ave',retail_gas])
+		filewriter.writerow(['AFS Weekly Diesel Fuel Price, 4654 W Washington Blvd',sale_gas])
+		
+	return sale_gas
+		
+def main():
+	
+	winnebago()
+	rd_diesel()
+
 
 if __name__ == '__main__':
     main()
