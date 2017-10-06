bd8f7765c9b8a35c96006d4807a7e2bdd2409773
darwady2 <darwady2@gmail.com>
2017-10-06 10:00:27 -0500

    updated script

--- chrisGasBuddy.py
+++ chrisGasBuddy.py
@@ -26,21 +26,34 @@ def show_prices(prices):
 	return complete_prices
 
 
+#Creates a unique filename with the proper date formatting.
+def create_filename(client):
+	current_date = datetime.datetime.now().strftime("%m-%d-%y")
+	return client + '_' + current_date + '.csv'
+
+
 #Winnebago logic.
 def winnebago():
+	
+	#Constants.
+	mft = .184
+	propane_mft = .226
+	
+	#Fetch price.
 	html = get_http('https://www.gasbuddy.com/Station/66970')
 	prices_list = get_prices_from_web(html)
 	price_final = show_prices(prices_list)
 	retail_gas = float(price_final[0])
-	mft = .184
-	propane_mft = .226
+	
+	#Calculate price.
 	retail_minus_mft = retail_gas - mft
 	sales_tax = .05 * retail_minus_mft
 	retail_taxfree = retail_minus_mft - sales_tax
 	propane_price = retail_taxfree - 0.50
 	raw_cost_of_propane = propane_price - propane_mft
-	current_date = datetime.datetime.now().strftime("%m-%d-%y")
-	filename = 'winnebago_price_' + current_date + '.csv'
+	
+	#Create CSV.
+	filename = create_filename('winnebago_price')
 	with open(filename, 'wb') as csvfile:
 		filewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 		filewriter.writerow(['Raw Cost of Propane',raw_cost_of_propane])
@@ -52,13 +65,18 @@ def winnebago():
 
 #R&D Diesel logic.
 def rd_diesel():
+	
+	#Fetch price.
 	html = get_http('https://www.gasbuddy.com/Station/29546')
 	prices_list = get_prices_from_web(html)
 	price_final = show_prices(prices_list)
 	retail_gas = float(price_final[3])
+	
+	#Calculate price.
 	sale_gas = retail_gas - .10
-	current_date = datetime.datetime.now().strftime("%m-%d-%y")
-	filename = 'r&d_diesel_price_' + current_date + '.csv'
+	
+	#Create CSV.
+	filename = create_filename('r&d_diesel_price')
 	with open(filename, 'wb') as csvfile:
 		filewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 		filewriter.writerow(['Retail Diesel at Falcon Fuel, 300 S Cicero Ave',retail_gas])
